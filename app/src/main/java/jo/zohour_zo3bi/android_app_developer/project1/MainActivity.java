00fa1919c1caacc82f8286142faa8e9c53cfae34
Zohour Al-Zou'bi <zalzoubi26@gmail.com>
2021-05-31 21:54:26 +0300

    Initial commit

---
+++ app/src/main/java/jo/zohour_zo3bi/android_app_developer/project1/MainActivity.java
@@ -0,0 +1,14 @@
+package jo.zohour_zo3bi.android_app_developer.project1;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
